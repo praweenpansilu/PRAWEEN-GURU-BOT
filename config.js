dc5f918fe99167495c72001ddffb39c8e21d4e39
praweenpansilu <159689243+praweenpansilu@users.noreply.github.com>
2024-02-11 15:23:12 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497078', 'me', true],
-  ['6281255369012', 'Ash', true], 
+  ['94762011703', 'me', true],
+  ['0762011703', 'Praween', true], 
   [''] 
 ] //Number of owners
 
